2c3b6d060609f9812d9c6b1ad4d4880374b00507
dalaocu <njumjy06@126.com>
2019-04-24 14:48:16 +0800

    lombok

--- src/main/java/org/fisco/bcos/autoconfigure/ServiceConfig.java
+++ src/main/java/org/fisco/bcos/autoconfigure/ServiceConfig.java
@@ -14,60 +14,19 @@ import lombok.Data;
 public class ServiceConfig {
 
     private String orgID;
-    private Integer connectSeconds= 30 ;
+    private Integer connectSeconds = 30;
     private Integer connectSleepPerMillis = 1;
     private GroupChannelConnectionsConfig allChannelConnections;
-    private static int groupId =1;
+    private static int groupId = 1;
 
     @Bean
     public Service getService(GroupChannelConnectionsConfig groupChannelConnectionsConfig) {
-    Service channelService= new Service();
-    channelService.setConnectSeconds(connectSeconds);
-    channelService.setOrgID(orgID);
-    channelService.setConnectSleepPerMillis(connectSleepPerMillis);
-    channelService.setGroupId(groupId);
-   channelService.setAllChannelConnections(groupChannelConnectionsConfig);
-    return channelService;
-    }
-
-
-    public String getOrgID() {
-        return orgID;
-    }
-
-    public void setOrgID(String orgID) {
-        this.orgID = orgID;
-    }
-
-    public Integer getConnectSeconds() {
-        return connectSeconds;
-    }
-
-    public void setConnectSeconds(Integer connectSeconds) {
-        this.connectSeconds = connectSeconds;
-    }
-
-    public Integer getConnectSleepPerMillis() {
-        return connectSleepPerMillis;
-    }
-
-    public void setConnectSleepPerMillis(Integer connectSleepPerMillis) {
-        this.connectSleepPerMillis = connectSleepPerMillis;
-    }
-
-    public GroupChannelConnectionsConfig getAllChannelConnections() {
-        return allChannelConnections;
-    }
-
-    public void setAllChannelConnections(GroupChannelConnectionsConfig allChannelConnections) {
-        this.allChannelConnections = allChannelConnections;
-    }
-
-    public static int getGroupId() {
-        return groupId;
-    }
-
-    public static void setGroupId(int groupId) {
-        ServiceConfig.groupId = groupId;
+        Service channelService = new Service();
+        channelService.setConnectSeconds(connectSeconds);
+        channelService.setOrgID(orgID);
+        channelService.setConnectSleepPerMillis(connectSleepPerMillis);
+        channelService.setGroupId(groupId);
+        channelService.setAllChannelConnections(groupChannelConnectionsConfig);
+        return channelService;
     }
 }
