9ba60e11de9587b5ad666ce3fa097ddc305a74ac
grayson <zhanglong04@baidu.com>
2019-04-30 17:41:35 +0800

    update code style

--- src/test/java/org/fisco/bcos/PrecompiledServiceApiTest.java
+++ src/test/java/org/fisco/bcos/PrecompiledServiceApiTest.java
@@ -3,37 +3,20 @@ package org.fisco.bcos;
 import static org.junit.Assert.assertTrue;
 
 import org.fisco.bcos.web3j.crypto.Credentials;
-import org.fisco.bcos.web3j.crypto.gm.GenCredential;
 import org.fisco.bcos.web3j.precompile.config.SystemConfigSerivce;
 import org.fisco.bcos.web3j.protocol.Web3j;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.fasterxml.jackson.databind.deser.Deserializers.Base;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
-public class PrecompiledServiceApiTest {
 
+public class PrecompiledServiceApiTest extends Base {
+
+    @Autowired
+    Web3j web3j;
+    
+    @Autowired
     private Credentials credentials;
-
-    @Before
-    public void setUp() throws Exception {
-        credentials =
-                GenCredential.create(
-                        "b83261efa42895c38c6c2364ca878f43e77f3cddbc922bf57d0d48070f79feb6");
-        if (credentials == null) {
-            throw new Exception("create Credentials failed");
-        }
-    }
-
-    @After
-    public void tearDown() {}
-
-    @Autowired Web3j web3j;
 
     @Test
     public void testSystemConfigService() throws Exception {
@@ -43,4 +26,5 @@ public class PrecompiledServiceApiTest {
         System.out.println(value);
         assertTrue("2000".equals(value));
     }
+
 }
--- src/test/java/org/fisco/bcos/Web3jApiTest.java
+++ src/test/java/org/fisco/bcos/Web3jApiTest.java
@@ -4,20 +4,19 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import lombok.extern.slf4j.Slf4j;
+
 import org.fisco.bcos.web3j.protocol.Web3j;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+import lombok.extern.slf4j.Slf4j;
+
+
 @Slf4j
-public class Web3jApiTest {
+public class Web3jApiTest extends BaseTest {
 
-    @Autowired Web3j web3j;
+    @Autowired
+    Web3j web3j;
 
     @Test
     public void getBlockNumber() throws IOException {
@@ -25,4 +24,5 @@ public class Web3jApiTest {
         log.info("blockNumber is {}", blockNumber);
         assertTrue(blockNumber.compareTo(new BigInteger("0")) >= 0);
     }
+
 }
