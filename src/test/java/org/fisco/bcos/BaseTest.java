26bcc7d916b6dfb37550477cfcd844eade993e1c
grayson <zhanglong04@baidu.com>
2019-05-16 23:59:54 +0800

    modify BaseTest to abstract class

--- src/test/java/org/fisco/bcos/BaseTest.java
+++ src/test/java/org/fisco/bcos/BaseTest.java
@@ -15,15 +15,10 @@
  */
 package org.fisco.bcos;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-public class BaseTest {
-
-    @Test
-    public void testBase() {}
-}
+public abstract class BaseTest {}
