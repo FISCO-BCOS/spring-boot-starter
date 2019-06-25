29cb654ac8647e4b7bc45441a90f46e9e1c8459c
fqliao <fqliao@yeah.net>
2019-06-25 10:38:57 +0800

    fix encryType config in yml and update sdk for 2.0.3

--- src/test/java/org/fisco/bcos/PrecompiledServiceApiTest.java
+++ src/test/java/org/fisco/bcos/PrecompiledServiceApiTest.java
@@ -3,7 +3,7 @@ package org.fisco.bcos;
 import static org.junit.Assert.assertTrue;
 
 import org.fisco.bcos.web3j.crypto.Credentials;
-import org.fisco.bcos.web3j.precompile.config.SystemConfigSerivce;
+import org.fisco.bcos.web3j.precompile.config.SystemConfigService;
 import org.fisco.bcos.web3j.protocol.Web3j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +16,7 @@ public class PrecompiledServiceApiTest extends BaseTest {
 
     @Test
     public void testSystemConfigService() throws Exception {
-        SystemConfigSerivce systemConfigSerivce = new SystemConfigSerivce(web3j, credentials);
+        SystemConfigService systemConfigSerivce = new SystemConfigService(web3j, credentials);
         systemConfigSerivce.setValueByKey("tx_count_limit", "2000");
         String value = web3j.getSystemConfigByKey("tx_count_limit").send().getSystemConfigByKey();
         System.out.println(value);
