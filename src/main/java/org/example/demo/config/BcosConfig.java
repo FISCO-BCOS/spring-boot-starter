b2fc0da4b0a7996e4be317f469528be741e55516
Kyon <32325790+kyonRay@users.noreply.github.com>
2022-03-14 09:46:06 +0800

    <fix>(project): adapt java sdk. (#101)

--- src/main/java/org/example/demo/config/BcosConfig.java
+++ src/main/java/org/example/demo/config/BcosConfig.java
@@ -3,7 +3,7 @@ package org.example.demo.config;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
-import org.fisco.bcos.sdk.config.model.AmopTopic;
+import org.fisco.bcos.sdk.v3.config.model.AmopTopic;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
